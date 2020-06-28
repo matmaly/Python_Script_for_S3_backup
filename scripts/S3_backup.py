03ce4e48dd38cf20292cceb7986c2fa2ae64f0b1
matmaly <mateuszfmaly@gmail.com>
2020-06-28 13:44:31 +0000

    Adding argparser Adding params to create_bucket with output from argparse

--- scripts/S3_backup.py
+++ scripts/S3_backup.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python3
 
 import boto3
+import argparse
 
-session = boto3.Session(profile_name="python_s3_backup")
+'''
+Create a bucket in S3 with a specified name in a specified region or default one.
+Argparser used to gather required input for bucket name and region
+'''
 
-s3_backup = session.client("s3")
+parser = argparse.ArgumentParser(description="Process S3 requests through command line")
+parser.add_argument("--region", "-r", help="Provide the region for the bucket")
+parser.add_argument("--name", "-n", required=True, help="Provide the name for the bucket")
 
-s3_backup.create_bucket(Bucket="testbucket.1235346347323ls")
+args = parser.parse_args()
+
+def create_bucket():
+    session = boto3.Session(profile_name="python_s3_backup")
+    if args.region is None:
+        s3_client = session.client("s3")
+        s3_client.create_bucket(Bucket=args.name)
+    else:
+        s3_client = session.client("s3", region_name=args.region)
+        location = {"LocationConstraint": args.region}
+        s3_client.create_bucket(Bucket=args.name, CreateBucketConfiguration=location)
+    
+create_bucket()
